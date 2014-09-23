9e170df9c8ffea0a3d4df9a920448b6fd338fd4f
Matthew Holloway <matthew.holloway@logicwerx.com>
2014-09-22 20:08:44 -0700

    Initial commit of app... will be integrating Rails/Angular for LSI app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LsiReports::Application.config.secret_key_base = '5701df3614c3bfcbe6fb98e6255849419fb8a4e2a56a0c123a264b1680743913865d7e987b98c12b4fc5544eac65c1bff98192b84c7509a81009da6c85c2d066'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LsiReports::Application.config.session_store :cookie_store, key: '_lsi_reports_session'
